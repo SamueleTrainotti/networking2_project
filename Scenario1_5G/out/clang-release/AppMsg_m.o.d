ba348ffba54bfb4ea207a519f53fa29b1d11d4bc
SamueleTrainotti <samueletr.iot@gmail.com>
2022-06-30 22:44:25 +0200

    Scenario 2 5G definitivo

---
+++ Scenario1_5G/out/clang-release/AppMsg_m.o.d
@@ -0,0 +1,110 @@
+out/clang-release//AppMsg_m.o: AppMsg_m.cc AppMsg_m.h \
+  /home/simu5g/omnetpp-6.0pre11/include/omnetpp.h \
+  ../inet4.3/src/inet/common/INETDefs_m.h \
+  ../inet4.3/src/inet/common/INETDefs.h \
+  ../inet4.3/src/inet/common/precompiled_release.h \
+  ../inet4.3/src/inet/common/precompiled.h \
+  ../inet4.3/src/inet/opp_defines.h ../inet4.3/src/inet/features.h \
+  ../inet4.3/src/inet/common/Compat.h \
+  ../inet4.3/src/inet/common/InitStages.h \
+  ../inet4.3/src/inet/common/Units_m.h \
+  ../inet4.3/src/inet/common/Units.h \
+  ../inet4.3/src/inet/common/INETMath.h \
+  ../inet4.3/src/inet/common/packet/chunk/Chunk_m.h \
+  ../inet4.3/src/inet/common/packet/chunk/BitCountChunk.h \
+  ../inet4.3/src/inet/common/packet/chunk/Chunk.h \
+  ../inet4.3/src/inet/common/IPrintableObject.h \
+  ../inet4.3/src/inet/common/MemoryInputStream.h \
+  ../inet4.3/src/inet/linklayer/common/MacAddress.h \
+  ../inet4.3/src/inet/networklayer/contract/ipv4/Ipv4Address.h \
+  ../inet4.3/src/inet/networklayer/contract/ipv6/Ipv6Address.h \
+  ../inet4.3/src/inet/common/MemoryOutputStream.h \
+  ../inet4.3/src/inet/common/Ptr.h \
+  ../inet4.3/src/inet/common/IntrusivePtr.h \
+  ../inet4.3/src/inet/common/TemporarySharedPtr.h \
+  ../inet4.3/src/inet/common/packet/tag/IRegionTaggedObject.h \
+  ../inet4.3/src/inet/common/packet/tag/SharingRegionTagSet.h \
+  ../inet4.3/src/inet/common/TagBase.h \
+  ../inet4.3/src/inet/common/packet/chunk/BitsChunk.h \
+  ../inet4.3/src/inet/common/packet/chunk/ByteCountChunk.h \
+  ../inet4.3/src/inet/common/packet/chunk/BytesChunk.h \
+  ../inet4.3/src/inet/common/packet/chunk/cPacketChunk.h \
+  ../inet4.3/src/inet/common/packet/chunk/EmptyChunk.h \
+  ../inet4.3/src/inet/common/packet/chunk/EncryptedChunk.h \
+  ../inet4.3/src/inet/common/packet/chunk/FieldsChunk.h \
+  ../inet4.3/src/inet/common/packet/chunk/SequenceChunk.h \
+  ../inet4.3/src/inet/common/packet/chunk/SliceChunk.h
+
+AppMsg_m.h:
+
+/home/simu5g/omnetpp-6.0pre11/include/omnetpp.h:
+
+../inet4.3/src/inet/common/INETDefs_m.h:
+
+../inet4.3/src/inet/common/INETDefs.h:
+
+../inet4.3/src/inet/common/precompiled_release.h:
+
+../inet4.3/src/inet/common/precompiled.h:
+
+../inet4.3/src/inet/opp_defines.h:
+
+../inet4.3/src/inet/features.h:
+
+../inet4.3/src/inet/common/Compat.h:
+
+../inet4.3/src/inet/common/InitStages.h:
+
+../inet4.3/src/inet/common/Units_m.h:
+
+../inet4.3/src/inet/common/Units.h:
+
+../inet4.3/src/inet/common/INETMath.h:
+
+../inet4.3/src/inet/common/packet/chunk/Chunk_m.h:
+
+../inet4.3/src/inet/common/packet/chunk/BitCountChunk.h:
+
+../inet4.3/src/inet/common/packet/chunk/Chunk.h:
+
+../inet4.3/src/inet/common/IPrintableObject.h:
+
+../inet4.3/src/inet/common/MemoryInputStream.h:
+
+../inet4.3/src/inet/linklayer/common/MacAddress.h:
+
+../inet4.3/src/inet/networklayer/contract/ipv4/Ipv4Address.h:
+
+../inet4.3/src/inet/networklayer/contract/ipv6/Ipv6Address.h:
+
+../inet4.3/src/inet/common/MemoryOutputStream.h:
+
+../inet4.3/src/inet/common/Ptr.h:
+
+../inet4.3/src/inet/common/IntrusivePtr.h:
+
+../inet4.3/src/inet/common/TemporarySharedPtr.h:
+
+../inet4.3/src/inet/common/packet/tag/IRegionTaggedObject.h:
+
+../inet4.3/src/inet/common/packet/tag/SharingRegionTagSet.h:
+
+../inet4.3/src/inet/common/TagBase.h:
+
+../inet4.3/src/inet/common/packet/chunk/BitsChunk.h:
+
+../inet4.3/src/inet/common/packet/chunk/ByteCountChunk.h:
+
+../inet4.3/src/inet/common/packet/chunk/BytesChunk.h:
+
+../inet4.3/src/inet/common/packet/chunk/cPacketChunk.h:
+
+../inet4.3/src/inet/common/packet/chunk/EmptyChunk.h:
+
+../inet4.3/src/inet/common/packet/chunk/EncryptedChunk.h:
+
+../inet4.3/src/inet/common/packet/chunk/FieldsChunk.h:
+
+../inet4.3/src/inet/common/packet/chunk/SequenceChunk.h:
+
+../inet4.3/src/inet/common/packet/chunk/SliceChunk.h:
---
+++ Scenario1_5G/out/clang-release/ServerApp.o.d
@@ -0,0 +1,184 @@
+out/clang-release//ServerApp.o: ServerApp.cc ServerApp.h \
+  ../inet4.3/src/inet/applications/udpapp/UdpBasicBurst.h \
+  ../inet4.3/src/inet/applications/base/ApplicationBase.h \
+  ../inet4.3/src/inet/common/lifecycle/ModuleOperations.h \
+  ../inet4.3/src/inet/common/lifecycle/LifecycleOperation.h \
+  ../inet4.3/src/inet/common/INETDefs.h \
+  ../inet4.3/src/inet/common/precompiled_release.h \
+  ../inet4.3/src/inet/common/precompiled.h \
+  /home/simu5g/omnetpp-6.0pre11/include/omnetpp.h \
+  ../inet4.3/src/inet/opp_defines.h ../inet4.3/src/inet/features.h \
+  ../inet4.3/src/inet/common/Compat.h \
+  ../inet4.3/src/inet/common/InitStages.h \
+  ../inet4.3/src/inet/common/lifecycle/OperationalBase.h \
+  ../inet4.3/src/inet/common/lifecycle/OperationalMixin.h \
+  ../inet4.3/src/inet/common/lifecycle/ILifecycle.h \
+  ../inet4.3/src/inet/transportlayer/contract/udp/UdpSocket.h \
+  ../inet4.3/src/inet/common/packet/Message.h \
+  ../inet4.3/src/inet/common/IPrintableObject.h \
+  ../inet4.3/src/inet/common/TagBase.h ../inet4.3/src/inet/common/Ptr.h \
+  ../inet4.3/src/inet/common/IntrusivePtr.h \
+  ../inet4.3/src/inet/common/Units.h \
+  ../inet4.3/src/inet/common/INETMath.h \
+  ../inet4.3/src/inet/common/packet/tag/ITaggedObject.h \
+  ../inet4.3/src/inet/common/packet/tag/SharingTagSet.h \
+  ../inet4.3/src/inet/common/packet/Packet.h \
+  ../inet4.3/src/inet/common/packet/chunk/BitsChunk.h \
+  ../inet4.3/src/inet/common/packet/chunk/Chunk.h \
+  ../inet4.3/src/inet/common/MemoryInputStream.h \
+  ../inet4.3/src/inet/linklayer/common/MacAddress.h \
+  ../inet4.3/src/inet/networklayer/contract/ipv4/Ipv4Address.h \
+  ../inet4.3/src/inet/networklayer/contract/ipv6/Ipv6Address.h \
+  ../inet4.3/src/inet/common/MemoryOutputStream.h \
+  ../inet4.3/src/inet/common/TemporarySharedPtr.h \
+  ../inet4.3/src/inet/common/packet/tag/IRegionTaggedObject.h \
+  ../inet4.3/src/inet/common/packet/tag/SharingRegionTagSet.h \
+  ../inet4.3/src/inet/common/packet/chunk/BytesChunk.h \
+  ../inet4.3/src/inet/common/packet/chunk/EmptyChunk.h \
+  ../inet4.3/src/inet/common/packet/chunk/SequenceChunk.h \
+  ../inet4.3/src/inet/common/packet/chunk/SliceChunk.h \
+  ../inet4.3/src/inet/common/socket/ISocket.h \
+  ../inet4.3/src/inet/networklayer/common/L3Address.h \
+  ../inet4.3/src/inet/networklayer/common/ModuleIdAddress.h \
+  ../inet4.3/src/inet/networklayer/common/ModulePathAddress.h \
+  ../inet4.3/src/inet/networklayer/contract/clns/ClnsAddress.h \
+  ../inet4.3/src/inet/networklayer/contract/IInterfaceTable.h \
+  ../inet4.3/src/inet/transportlayer/contract/udp/UdpControlInfo.h \
+  ../inet4.3/src/inet/transportlayer/contract/udp/UdpControlInfo_m.h \
+  ../inet4.3/src/inet/common/INETDefs_m.h \
+  ../inet4.3/src/inet/networklayer/common/IpProtocolId_m.h \
+  ../inet4.3/src/inet/networklayer/common/L3Address_m.h \
+  ../inet4.3/src/inet/applications/tcpapp/GenericAppMsg_m.h \
+  ../inet4.3/src/inet/common/Units_m.h \
+  ../inet4.3/src/inet/common/packet/chunk/Chunk_m.h \
+  ../inet4.3/src/inet/common/packet/chunk/BitCountChunk.h \
+  ../inet4.3/src/inet/common/packet/chunk/ByteCountChunk.h \
+  ../inet4.3/src/inet/common/packet/chunk/cPacketChunk.h \
+  ../inet4.3/src/inet/common/packet/chunk/EncryptedChunk.h \
+  ../inet4.3/src/inet/common/packet/chunk/FieldsChunk.h \
+  ../inet4.3/src/inet/common/TimeTag_m.h \
+  ../inet4.3/src/inet/common/TagBase_m.h
+
+ServerApp.h:
+
+../inet4.3/src/inet/applications/udpapp/UdpBasicBurst.h:
+
+../inet4.3/src/inet/applications/base/ApplicationBase.h:
+
+../inet4.3/src/inet/common/lifecycle/ModuleOperations.h:
+
+../inet4.3/src/inet/common/lifecycle/LifecycleOperation.h:
+
+../inet4.3/src/inet/common/INETDefs.h:
+
+../inet4.3/src/inet/common/precompiled_release.h:
+
+../inet4.3/src/inet/common/precompiled.h:
+
+/home/simu5g/omnetpp-6.0pre11/include/omnetpp.h:
+
+../inet4.3/src/inet/opp_defines.h:
+
+../inet4.3/src/inet/features.h:
+
+../inet4.3/src/inet/common/Compat.h:
+
+../inet4.3/src/inet/common/InitStages.h:
+
+../inet4.3/src/inet/common/lifecycle/OperationalBase.h:
+
+../inet4.3/src/inet/common/lifecycle/OperationalMixin.h:
+
+../inet4.3/src/inet/common/lifecycle/ILifecycle.h:
+
+../inet4.3/src/inet/transportlayer/contract/udp/UdpSocket.h:
+
+../inet4.3/src/inet/common/packet/Message.h:
+
+../inet4.3/src/inet/common/IPrintableObject.h:
+
+../inet4.3/src/inet/common/TagBase.h:
+
+../inet4.3/src/inet/common/Ptr.h:
+
+../inet4.3/src/inet/common/IntrusivePtr.h:
+
+../inet4.3/src/inet/common/Units.h:
+
+../inet4.3/src/inet/common/INETMath.h:
+
+../inet4.3/src/inet/common/packet/tag/ITaggedObject.h:
+
+../inet4.3/src/inet/common/packet/tag/SharingTagSet.h:
+
+../inet4.3/src/inet/common/packet/Packet.h:
+
+../inet4.3/src/inet/common/packet/chunk/BitsChunk.h:
+
+../inet4.3/src/inet/common/packet/chunk/Chunk.h:
+
+../inet4.3/src/inet/common/MemoryInputStream.h:
+
+../inet4.3/src/inet/linklayer/common/MacAddress.h:
+
+../inet4.3/src/inet/networklayer/contract/ipv4/Ipv4Address.h:
+
+../inet4.3/src/inet/networklayer/contract/ipv6/Ipv6Address.h:
+
+../inet4.3/src/inet/common/MemoryOutputStream.h:
+
+../inet4.3/src/inet/common/TemporarySharedPtr.h:
+
+../inet4.3/src/inet/common/packet/tag/IRegionTaggedObject.h:
+
+../inet4.3/src/inet/common/packet/tag/SharingRegionTagSet.h:
+
+../inet4.3/src/inet/common/packet/chunk/BytesChunk.h:
+
+../inet4.3/src/inet/common/packet/chunk/EmptyChunk.h:
+
+../inet4.3/src/inet/common/packet/chunk/SequenceChunk.h:
+
+../inet4.3/src/inet/common/packet/chunk/SliceChunk.h:
+
+../inet4.3/src/inet/common/socket/ISocket.h:
+
+../inet4.3/src/inet/networklayer/common/L3Address.h:
+
+../inet4.3/src/inet/networklayer/common/ModuleIdAddress.h:
+
+../inet4.3/src/inet/networklayer/common/ModulePathAddress.h:
+
+../inet4.3/src/inet/networklayer/contract/clns/ClnsAddress.h:
+
+../inet4.3/src/inet/networklayer/contract/IInterfaceTable.h:
+
+../inet4.3/src/inet/transportlayer/contract/udp/UdpControlInfo.h:
+
+../inet4.3/src/inet/transportlayer/contract/udp/UdpControlInfo_m.h:
+
+../inet4.3/src/inet/common/INETDefs_m.h:
+
+../inet4.3/src/inet/networklayer/common/IpProtocolId_m.h:
+
+../inet4.3/src/inet/networklayer/common/L3Address_m.h:
+
+../inet4.3/src/inet/applications/tcpapp/GenericAppMsg_m.h:
+
+../inet4.3/src/inet/common/Units_m.h:
+
+../inet4.3/src/inet/common/packet/chunk/Chunk_m.h:
+
+../inet4.3/src/inet/common/packet/chunk/BitCountChunk.h:
+
+../inet4.3/src/inet/common/packet/chunk/ByteCountChunk.h:
+
+../inet4.3/src/inet/common/packet/chunk/cPacketChunk.h:
+
+../inet4.3/src/inet/common/packet/chunk/EncryptedChunk.h:
+
+../inet4.3/src/inet/common/packet/chunk/FieldsChunk.h:
+
+../inet4.3/src/inet/common/TimeTag_m.h:
+
+../inet4.3/src/inet/common/TagBase_m.h:
